276297503d828ac8a8e542ea07e8dbb275938517
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-07-13 15:51:44 -0400

    Begin setting up database

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,36 @@
 //setup data structures
+const database = {
+    //orderBuilder: {},
+    colors: [
+        { id: 1, color: "Silver", price: 500 },
+        { id: 2, color: "Midnight Blue", price: 710 },
+        { id: 3, color: "Firebrick Red", price: 965 },
+        {//color4}
+    ],
+//     sizes: [
+//         { id: 1, carets: 0.5, price: 405 },
+//         { id: 2, carets: 0.75, price: 782 },
+//         { id: 3, carets: 1, price: 1470 },
+//         { id: 4, carets: 1.5, price: 1997 },
+//         { id: 5, carets: 2, price: 3638 }
+//     ],
+//     metals: [
+//         { id: 1, metal: "Sterling Silver", price: 12.42 },
+//         { id: 2, metal: "14K Gold", price: 736.4 },
+//         { id: 3, metal: "24K Gold", price: 1258.9 },
+//         { id: 4, metal: "Platinum", price: 795.45 },
+//         { id: 5, metal: "Palladium", price: 1241.0 }
+//     ],
+//     customOrders: [
+//         {
+//             id: 1,
+//             metalId: 3,
+//             sizeId: 2,
+//             styleId: 3,
+//             timestamp: 1614659931693
+//         }
+//     ],
+// }
 
 
 //set up get functions
